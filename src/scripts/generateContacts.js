ff35ef3d943840f24dd987f7c4e4ca619f62a1e9
Natalya Malenko <nataljuna@gmail.com>
2025-05-03 18:52:21 +0300

    added files

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,20 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-generateContacts(5);
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+    let newContact = [];
+
+    for (let i = 0; i < number; i++) {
+      newContact.push(createFakeContact());
+    }
+
+    await writeContacts([...contacts, ...newContact]);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+generateContacts(10);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const json = JSON.stringify(updatedContacts, null, 2);
+    await fs.writeFile(PATH_DB, json, 'utf-8');
+    console.log('Контакти успішно записані у файл.');
+  } catch (error) {
+    console.error('Помилка запису:', error.message);
+  }
+};
